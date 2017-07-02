5a947802267ac53556d87f879ed39174caf183c5
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2017-07-02 17:59:43 -0600

    Added header to Products view

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+import header from './assets/slim_fom_mick.jpg';
 import './Products.css';
 
 class Products extends Component {
     render() {
         return (
             <div>
+                <img src={header} />
                 Product Component
             </div>
         );
