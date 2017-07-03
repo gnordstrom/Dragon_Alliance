6a9c384028fa55d5d4ae9bcceb5428bcba1b4cf0
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2017-07-03 12:14:12 -0600

    Added header image for Team component

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+import team from './assets/ace-header.jpg';
 import './Team.css';
 
 class Team extends Component {
     render() {
         return (
             <div>
+                <img src={team}/>
                 Team Component
             </div>
         );
