a6151a5f3f53b7c3762a761b167874ac6c2bdb2e
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2017-07-03 16:45:43 -0600

    Finished styling for glasses

--- src/components/CrossPerformance/AdaptiveTech/AdaptiveTech.js
+++ src/components/CrossPerformance/AdaptiveTech/AdaptiveTech.js
@@ -50,15 +50,36 @@ class AdaptiveTech extends Component {
                                 <span className="item-content-logo-hydra"></span>
                                 <span className="item-content-content">HYDRA - </span> 
                                 <p className="item-bottom-title">WATERMANX</p>
-
                             </div>
                         </div>
                     </div>
 
+                    <hr />
 
                     <div className="glasses-row">
+                        <div className="glasses-item left">
+                            <div className="item-image-wrapper">
+                                <img src={glasses3} className="item-image-image"/>
+                            </div>
+                            <div className="item-content">
+                                <span className="item-content-logo-terra"></span>
+                                <span className="item-content-content">TERRA - </span> 
+                                <p className="item-bottom-title">ENDUROX</p>
+                            </div>
+                        </div>
 
+                        <div className="glasses-item">
+                            <div className="item-image-wrapper">
+                                <img src={glasses4} className="item-image-image"/>
+                            </div>
+                            <div className="item-content">
+                                <span className="item-content-logo-glacia"></span>
+                                <span className="item-content-content">GLACIA - </span> 
+                                <p className="item-bottom-title">MOUNTAINEERX</p>
+                            </div>
+                        </div>
                     </div>
+
                 </div>
             </div>
         );
