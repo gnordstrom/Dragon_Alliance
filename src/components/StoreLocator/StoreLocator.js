fa24df70eb3fd3bb434e6803f86763c1965dbfe3
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2017-07-03 12:08:07 -0600

    Added image for Store Locator header

--- src/components/StoreLocator/StoreLocator.js
+++ src/components/StoreLocator/StoreLocator.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
+import header from './assets/jossi-header.jpg'
+
 import './StoreLocator.css';
 
 class StoreLocator extends Component {
     render() {
         return (
             <div>
+                <div className="header-wrapper"> 
+                    <img src={header}/>
+                </div>
                 Store Locator Component!
             </div>
         );
