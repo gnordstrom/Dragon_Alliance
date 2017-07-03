be09ddcf7ec06044474116c248781688dc19b388
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2017-07-03 15:43:39 -0600

    Completed feature for adaptive-content2

--- src/components/CrossPerformance/AdaptiveTech/AdaptiveTech.js
+++ src/components/CrossPerformance/AdaptiveTech/AdaptiveTech.js
@@ -28,13 +28,22 @@ class AdaptiveTech extends Component {
 
                 <div className="adaptive-content2">
                     <div className="content2-card">
-                        <div className="glacia"></div>
+                        <div className="card glacia"></div>
+                        <div className="card-content">
+                            On snow, the MountaineerX is built to adapt with an interchangeable C-Temple to secure fit, leather side sun shields to reduce glare, and a Rope Lanyard system that reduces the risk of loss.
+                        </div>
                     </div>
                     <div className="content2-card">
-                        <div className="terra"></div>
+                        <div className="card terra"></div>
+                        <div className="card-content">
+                            On land, the EnduroX is built to adapt with fully adjustable temples and nosebridge, Transitions® lenses that filter light as users move from bright sun to shade. An extra set of clear lenses for night/low light activities are included.
+                        </div>
                     </div>
                     <div className="content2-card">
-                        <div className="hydra"></div>
+                        <div className="card hydra"></div>
+                        <div className="card-content">
+                            On water, the WatermanX and SeafarerX are built to adapt with an integrated Aqua Leash system which secures fit and floatable frames reducing the risk of loss.
+                        </div>
                     </div>
                 </div>
             </div>
