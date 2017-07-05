6fa8fb09b91a794e5fa618a11ba6da0050af727e
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2017-07-05 10:36:40 -0600

    Added PerformanceTech component

--- src/components/CrossPerformance/PerformanceTech/PerformanceTech.js
+++ src/components/CrossPerformance/PerformanceTech/PerformanceTech.js
@@ -7,8 +7,13 @@ import predatorLogo from './assets/performance_lens_technology.svg';
 
 const PerformanceTech = props => {
     return (
-        <div>
-            <img src={predatorLogo} alt="Predator Performance"/>
+        <div className="predator-container">
+            <div className="predator-wrapper">
+                <img className="predator-logo" src={predatorLogo} alt="Predator Performance"/>
+            </div>
+            <div className="predator-wrapper predator-content">
+                Performance Lens Technology works hard so that consumers can fully immerse themselves in the moment, allowing instinct and passion to lead to clearer and more vivid outdoor experiences. They not only provide maximum protection from sun UV, but also corrosional elements such as salt water, sun block, fish guts, and equipment impact.
+            </div>
         </div>
     );
 };
